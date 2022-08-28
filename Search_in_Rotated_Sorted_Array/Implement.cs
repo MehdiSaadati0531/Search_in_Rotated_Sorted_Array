b53173686870e554b2348c7f973d67676cb34fc4
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-28 11:26:30 +0430

    Add files via upload

---
+++ Search_in_Rotated_Sorted_Array/Implement.cs
@@ -0,0 +1,51 @@
+﻿namespace Search_in_Rotated_Sorted_Array;
+
+public class Bs
+{
+    public int Search(int[] nums, int target)
+    {
+        int first = 0;
+        int last = nums.Length - 1;
+        int middle = (first + last) / 2;
+        while (true)
+        {
+            if (target == nums[middle])
+            {
+                return middle;
+            }
+            else if (first > last)
+            {
+                return -1;
+            }
+            else if (nums[first] <= nums[middle])
+            {
+                if (target >= nums[first] && target < nums[middle])
+                {
+                    last = middle - 1;
+                    middle = (first + last) / 2;
+                }
+                else
+                {
+                    first = middle + 1;
+                    middle = (first + last) / 2;
+                }
+            }
+            else
+            {
+                if (target > nums[middle] && target <= nums[last])
+                {
+                    first = middle + 1;
+                    middle = (first + last) / 2;
+                }
+                else
+                {
+                    last = middle - 1;
+                    middle = (first + last) / 2;
+                    int hello = 0;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Search_in_Rotated_Sorted_Array/Program.cs
@@ -0,0 +1,8 @@
+﻿using Search_in_Rotated_Sorted_Array;
+
+int[] array ={ 2, 3, 4, 5, 6, 7, 8, 9, 1 };
+
+Bs a = new Bs();
+
+
+Console.WriteLine(a.Search(array, 9));
